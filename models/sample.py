b3e90515d70bc1e7ab137f31cac141356f758b0b
TheGravityFalls-11 <hotwaniaastha25@gmail.com>
2025-04-08 19:30:42 +0530

    Update sample.py

--- models/sample.py
+++ models/sample.py
@@ -1,6 +1,1 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
 
-tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-
-print("Model and tokenizer loaded successfully! 🎉")
